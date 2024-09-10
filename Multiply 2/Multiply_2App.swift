29ed2b3f530c489ea341b857325308f52b5bfeee
Nolan <nolanbm2009@icloud.com>
2024-09-10 09:53:03 -0500

    Initial Commit

---
+++ Multiply 2/Multiply_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Multiply_2App.swift
+//  Multiply 2
+//
+//  Created by Nolan Brian McSweeney on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Multiply_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
